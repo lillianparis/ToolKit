f8a0b0c2d654b45132cae157a9f3e6819fcddc16
Lillian Paris <lillian.paris529@gmail.com>
2021-11-15 10:14:59 -0700

    Last of this branch

--- app/src/components/Navbar/navbar.jsx
+++ app/src/components/Navbar/navbar.jsx
@@ -5,18 +5,20 @@ function Navbar() {
     return (
 
 
-<nav>
-<div class="nav-wrapper">
-  <a href="#" class="brand-logo right">Logo</a>
-  <ul id="nav-mobile" class="left hide-on-med-and-down">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">JavaScript</a></li>
-  </ul>
-</div>
-</nav>
+        <nav>
+            {/* <nav class="#607d8b blue-grey" /> */}
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo right">Logo</a>
+                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
 
-)}
+    )
+}
 
 
 export default Navbar;
