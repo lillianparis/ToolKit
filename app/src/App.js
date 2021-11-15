f6a281d2eaec5dd1f1ebdca475bfe5783212c424
Lillian Paris <lillian.paris529@gmail.com>
2021-11-15 09:53:16 -0700

    Got homepage started

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/navbar';
+import Landing from "./pages/Landing/landing";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <Navbar />
+      <Router>
+        <Switch>
+
+          <Route exact path="/" component={Landing} />
+          {/* <Route exact path="/developments" component={developments} />
+          <Route exact path="/inquiry" component={inquiry} />
+          <Route exact path="/about" component={about} /> */}
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ app/src/pages/Landing/landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Landing = () => {
+    return (
+
+        <a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+    )
+
+}
+
+export default Landing;
