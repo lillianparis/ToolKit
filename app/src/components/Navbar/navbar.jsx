614c195b4e33f7454c1102ba6b3e34509237d2c1
Lillian Paris <lillian.paris529@gmail.com>
2021-12-07 10:08:00 -0700

    Changing fonts

--- app/src/components/Navbar/navbar.jsx
+++ app/src/components/Navbar/navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import "../Navbar/navbar.scss"
 
 
 function Navbar() {
     return (
 
 
-<nav>
-<div class="nav-wrapper">
-  <a href="#" class="brand-logo right">Logo</a>
-  <ul id="nav-mobile" class="left hide-on-med-and-down">
-    <li><a href="sass.html">Sass</a></li>
-    <li><a href="badges.html">Components</a></li>
-    <li><a href="collapsible.html">JavaScript</a></li>
-  </ul>
-</div>
-</nav>
+      <nav class="#00695c teal darken-3">
+      <div class="nav-wrapper" >
+        <a href="#" class="brand-logo center">Progressive Toolkit</a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+          <li><a href="sass.html">Suggestions</a></li>
+          <li><a href="badges.html">Contact</a></li>
+          <li><a href="collapsible.html">Apps</a></li>
+        </ul>
+      </div>
+    </nav>
 
 )}
 
--- app/src/pages/Landing/landing.jsx
+++ app/src/pages/Landing/landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import M from "materialize-css";
 import { useEffect } from "react";
+import "../Landing/landing.scss";
 
 
 // useEffect tells React to do something after render. 
@@ -15,10 +16,10 @@ const Landing = () => {
         <div class="parallax-container">
         <div class="parallax"><img alt="Organization" src="https://images.unsplash.com/photo-1611079829529-fe31a9531295?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/></div>
       </div>
-      <div class="section white">
+      <div class="section #6d4c41 brown darken-1">
         <div class="row container">
-          <h2 class="header">Parallax</h2>
-          <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+          <h2 class="header">Progressive Toolkit</h2>
+          <p class="grey-text text-darken-3 lighten-3" class="color">This application features several helpful tools to make your life easier! </p>
         </div>
       </div>
       <div class="parallax-container">
