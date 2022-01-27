a6905945b5b0c9dd92ea04080b874cb4d75415e1
Lillian Paris <lillian.paris529@gmail.com>
2022-01-27 10:59:28 -0700

    TEST

--- app/src/pages/Calendar/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Calendar = () => {
-    return (
-        <h1>hi</h1>
-    )
-}
-
-export default Calendar;
--- app/src/pages/Notepad/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Notepad = () => {
-    return (
-<h1>hiiiiiii</h1>
-    )
-}
-
-export default Notepad;
